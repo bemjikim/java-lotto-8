31930d6e89423660d7f87a119d02c881865f72e8
BumjinKim <21900104@handong.ac.kr>
2025-11-02 00:54:31 +0900

    feat(enums): LottoInputMessage 클래스 구조 생성 및 기능 구현 - 입력 메시지를 출력하는 메서드 작성

---
+++ src/main/java/lotto/enums/LottoInputMessage.java
@@ -0,0 +1,15 @@
+package lotto.enums;
+
+public enum LottoInputMessage {
+    CASH_INPUT_MESSAGE("구입금액을 입력해 주세요."),
+    NUMBER_INPUT_MESSAGE("당첨 번호를 입력해 주세요."),
+    BONUS_NUMBER_INPUT_MESSAGE("보너스 번호를 입력해주세요.");
+
+    private final String message;
+
+    private LottoInputMessage(String message) {
+        this.message = message;
+    }
+
+    public void printMessage() { System.out.print(message); }
+}
