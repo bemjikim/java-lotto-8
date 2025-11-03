cfc2d02bfa334137d2d251f0a8c872c454764b20
BumjinKim <21900104@handong.ac.kr>
2025-11-03 20:23:37 +0900

    feat(domain): Money 클래스 기능 추가 및 리팩토링 - 시도 횟수인 tryCount 추가 - getter 메서드 추가 - 1000원으로 나누어떨어지는지 검증 메서드 구현

--- src/main/java/lotto/domain/Money.java
+++ src/main/java/lotto/domain/Money.java
@@ -1,16 +1,30 @@
 package lotto.domain;
 
-import java.util.List;
+import lotto.enums.ErrorMessage;
 
 public class Money {
-    private final int money;
+    private final int cash;
+    private final int tryCount;
 
-    public Money(int money) {
-        validate(money);
-        this.money = money;
+    public Money(int cash, int tryCount) {
+        validateIsDivisible(cash);
+        this.cash = cash;
+        this.tryCount = cash / 1000;
     }
 
-    private void validate(int money) {
-        //TODO: 1000원으로 나누어 떨어지는지 검증
+    public int getTryCount() {
+        return tryCount;
+    }
+
+    public int getCash() {
+        return cash;
+    }
+
+    private void validateIsDivisible(int cash) {
+        if (cash % 1000 == 0) {
+            return;
+        }
+
+        throw new IllegalArgumentException(ErrorMessage.INVALID_NONE_DIVISIBLE.getMessage() + cash);
     }
 }
