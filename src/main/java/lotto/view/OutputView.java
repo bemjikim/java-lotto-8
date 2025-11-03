a6b702ef63ba53d667c605d43b03a3789d4e8a31
BumjinKim <21900104@handong.ac.kr>
2025-11-03 20:30:52 +0900

    feat(view): OutputView 클래스 구조 생성 - 예외처리 메시지 출력 메서드 구현 - 몇개 구입했는지 알려주는 로또 메시지 메서드 구현 - 구입 로또 번호 출력 메서드 구현 - 몇개 당첨됐는지 출력하는 메서드 구현

--- src/main/java/lotto/view/OutputView.java
+++ src/main/java/lotto/view/OutputView.java
@@ -1,5 +1,49 @@
 package lotto.view;
 
+import lotto.enums.LottoOutputMessage;
+
+import java.util.Arrays;
+import java.util.List;
+
+import static lotto.enums.LottoOutputMessage.*;
+
 public class OutputView {
-    //TODO: 출력 로직 구현
+    public void printException(IllegalArgumentException e) {
+        printMessage(e.getMessage());
+    }
+
+    public static void printPurchaseLottoMessage(int tryCount) {
+        printMessage(String.format(NUMBER_OF_BUY_OUTPUT_MESSAGE.getMessage(), tryCount));
+    }
+
+    public static void printPurchaseLottoNumber(List<Integer> numbers) {
+        printMessage(numbers.toString());
+    }
+
+    /**
+     * 로또 결과를 출력하는 함수
+     *
+     * @param compareResult: 각 구매로또와 당첨로또에서 매칭된 숫자를 가진 리스트
+     * @param totalCash: 사용자가 입력한 금액
+     */
+    public static void printLottoStatistics(List<Integer> compareResult, int totalCash) {
+
+        printMessage(RESULT_STATISTICS_OUTPUT_MESSAGE.getMessage());
+
+        long totalWinning = Arrays.stream(LottoOutputMessage.values())
+                .mapToLong(msg -> msg.calculateWinningMoney(compareResult))
+                .sum();
+
+        double profitRate = (double) totalWinning / totalCash * 100;
+
+        printMatchResult(String.format(RESULT_OUTPUT_MESSAGE.getMessage(), profitRate));
+    }
+
+    public static void printMatchResult(String matchResult) {
+        printMessage(matchResult);
+    }
+
+    private static void printMessage(String message) {
+        System.out.println(message);
+    }
 }
