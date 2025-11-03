4616979ab5268d094c1f568cee6ba4f52676d7e1
BumjinKim <21900104@handong.ac.kr>
2025-11-03 20:39:14 +0900

    refactor(enums): LottoInputMessage 리팩토링 - 개행문자 \n 추가

--- src/main/java/lotto/enums/LottoInputMessage.java
+++ src/main/java/lotto/enums/LottoInputMessage.java
@@ -2,8 +2,8 @@ package lotto.enums;
 
 public enum LottoInputMessage {
     CASH_INPUT_MESSAGE("구입금액을 입력해 주세요."),
-    NUMBER_INPUT_MESSAGE("당첨 번호를 입력해 주세요."),
-    BONUS_NUMBER_INPUT_MESSAGE("보너스 번호를 입력해주세요.");
+    NUMBER_INPUT_MESSAGE("\n당첨 번호를 입력해 주세요."),
+    BONUS_NUMBER_INPUT_MESSAGE("\n보너스 번호를 입력해주세요.");
 
     private final String message;
 
@@ -11,5 +11,7 @@ public enum LottoInputMessage {
         this.message = message;
     }
 
-    public void printMessage() { System.out.print(message); }
+    public void printMessage() {
+        System.out.println(message);
+    }
 }
