091a15cd293bb74bea417a23120816658a0378f3
BumjinKim <21900104@handong.ac.kr>
2025-11-03 20:19:25 +0900

    remove(service,test): LottoTest, LottoService, MoneyService 클래스 삭제

--- src/main/java/lotto/service/LottoService.java
+++
@@ -1,5 +0,0 @@
-package lotto.service;
-
-public class LottoService {
-    // TODO: Lotto 프로세스 실행 및 조율 로직 구현
-}
--- src/main/java/lotto/service/MoneyService.java
+++
@@ -1,5 +0,0 @@
-package lotto.service;
-
-public class MoneyService {
-    // TODO: Money 프로세스 실행 및 조율 로직 구현
-}
