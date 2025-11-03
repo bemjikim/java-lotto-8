4c850546bbe3868dcdb3756296bf17564447b4a9
BumjinKim <21900104@handong.ac.kr>
2025-11-03 20:26:05 +0900

    refactor(config): appConfig 클래스 리팩토링 - memoryPurchaseLottoRepository 의존성 추가 주입 - memoryWinningLottoRepository 의존성 추가 주입

--- src/main/java/lotto/config/AppConfig.java
+++ src/main/java/lotto/config/AppConfig.java
@@ -1,6 +1,8 @@
 package lotto.config;
 
 import lotto.controller.LottoController;
+import lotto.repository.MemoryPurchaseLottoRepository;
+import lotto.repository.MemoryWinningLottoRepository;
 import lotto.util.LottoNumberGeneratorImpl;
 import lotto.view.InputView;
 import lotto.view.OutputView;
@@ -13,14 +15,22 @@ public class AppConfig {
     private final InputView inputView;
     private final OutputView outputView;
     private final LottoNumberGeneratorImpl lottoNumberGeneratorImpl;
+    private final MemoryPurchaseLottoRepository memoryPurchaseLottoRepository;
+    private final MemoryWinningLottoRepository memoryWinningLottoRepository;
 
     public AppConfig() {
         this.inputView = new InputView();
         this.outputView = new OutputView();
         this.lottoNumberGeneratorImpl = new LottoNumberGeneratorImpl();
+        this.memoryPurchaseLottoRepository = new MemoryPurchaseLottoRepository();
+        this.memoryWinningLottoRepository = new MemoryWinningLottoRepository();
     }
 
+    /**
+     * 의존성 주입 메서드
+     * @return: LottoController 의존성 주입
+     */
     public LottoController settingLottoController() {
-        return new LottoController(inputView, outputView, lottoNumberGeneratorImpl);
+        return new LottoController(inputView, outputView, lottoNumberGeneratorImpl, memoryPurchaseLottoRepository, memoryWinningLottoRepository);
     }
 }
